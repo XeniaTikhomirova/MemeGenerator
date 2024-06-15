2c819b7db707ad6ccc0515d24f84cfe91b3ac767
Kseniia <xeniiatikhomirova@gmail.com>
2024-06-15 21:35:32 +0400

    updated hooks in Form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -3,38 +3,44 @@ import styles from "./form.module.css";
 import Meme from "./Meme";
 
 export default function Form() {
-  let counter = 1;
-  const [text1, setText1] = useState("");
-  const [text2, setText2] = useState("");
-  const [image, setImage] = useState("1");
-  const [advice, setAdvice] = useState("");
-
   const URL = "https://api.adviceslip.com/advice";
-
   async function fetchUrl() {
     const res = await fetch(`${URL}?advice=string`);
     const data = await res.json();
-    setAdvice(data.slip.advice);
+    setMeme((prevValue) => {
+      return {
+        ...prevValue,
+        advice: data.slip.advice,
+      };
+    });
   }
-
   useEffect(() => {
     fetchUrl();
   }, []);
 
-  function handleGetImage() {
-    // counter = counter < 4 ? ((Number(image)) + 1) : 1
-    if (Number(image) < 3) {
-      counter = Number(image) + 1;
-    } else {
-      counter = 1;
-    }
-    console.log(counter);
-    setImage(counter);
-  }
+  //const [image, setImage] = useState("1");
+  //const [advice, setAdvice] = useState("");
 
-  function handleGetAdice() {
-    console.log("Random advice is clicked!");
-    fetchUrl();
+  const [meme, setMeme] = useState({
+    text: {
+      topText: "Type here your text or choose random one",
+      bottomText: "Type here your text or choose random one",
+      advice: "",
+    },
+    image: {
+      id: 1,
+      src: `src/public/images/memes/1.png`,
+    },
+  });
+
+  function addCustomText() {
+    setMeme((prevValue) => {
+      return {
+        ...prevValue,
+        topText: "Hello",
+        bottomText: "Beauty",
+      };
+    });
   }
 
   return (
@@ -55,19 +61,40 @@ export default function Form() {
             placeholder="Put some text..."
           />
 
+          <button onClick={() => addCustomText()} className={styles.formBtn}>
+            Add your text to the Meme
+          </button>
+
           <button
-            onClick={(evt) => handleGetImage(evt)}
+            onClick={() =>
+              setMeme((prevValue) => {
+                return {
+                  ...prevValue,
+                  image: {
+                    id: prevValue.image.id < 3 ? prevValue.image.id + 1 : 1,
+                    src: `src/public/images/memes/${
+                      prevValue.image.id < 3 ? prevValue.image.id + 1 : 1
+                    }.png`,
+                  },
+                };
+              })
+            }
             className={styles.formBtn}
           >
             Get a random Meme Image
           </button>
 
-          <button onClick={() => handleGetAdice()} className={styles.formBtn}>
+          <button onClick={() => fetchUrl()} className={styles.formBtn}>
             Get a random advice
           </button>
         </div>
       </main>
-      <Meme image={image} advice={advice} />
+      <Meme
+        image={meme.image}
+        advice={meme.text.advice}
+        topText={meme.text.topText}
+        bottomText={meme.text.bottomText}
+      />
     </div>
   );
 }
--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,13 +1,27 @@
 import { useEffect, useState } from "react";
 import styles from "./meme.module.css";
 
-export default function Meme({ image, advice }) {
+export default function Meme({ image, advice, topText, bottomText }) {
+  //  return (
+  //    <div className={styles.imgContainer}>
+  //      <h3 className={styles.memeText}>{advice}</h3>
+  //      <img
+  //        src={`src/public/images/memes/${image}.png`}
+  //        height="370px"
+  //        width="370px"
+  //        className={styles.imageMeme}
+  //      />
+  //    </div>
+  //  );
+
 
   return (
     <div className={styles.imgContainer}>
-      <h3 className={styles.memeText}>{advice}</h3>
+      <h3 className={styles.memeAdviceText}>{advice}</h3>
+      <h3 className={styles.memeTopText}>{topText}</h3>
+      <h3 className={styles.memeBottomText}>{bottomText}</h3>
       <img
-        src={`src/public/images/memes/${image}.png`}
+        src={`src/public/images/memes/1.png`}
         height="370px"
         width="370px"
         className={styles.imageMeme}
